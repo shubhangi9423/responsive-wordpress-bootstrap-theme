ee4031d486152252993931703fa8d7df168035b8
Shubangi Wani <ShubangiW@CCMT.local>
2017-11-07 18:50:14 +0530

    added all files

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+ <hr>
+
+      <footer>
+        <p>© Company 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+   <?php wp_footer(); ?>
+
+  </body>
+</html>
